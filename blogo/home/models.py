c269224d9e3af48a749688a273209429fe0a4095
nipun014 <145190298+nipun014@users.noreply.github.com>
2024-09-16 11:27:42 +0530

    Link blog post to a user

--- blogo/home/models.py
+++ blogo/home/models.py
@@ -1,11 +1,12 @@
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
