8e58f1ca5a2cd4580c132ba3cdbf86f9eeb33a35
nipun014 <145190298+nipun014@users.noreply.github.com>
2024-09-16 14:00:17 +0530

    Update models.py

--- blogo/home/models.py
+++ blogo/home/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
