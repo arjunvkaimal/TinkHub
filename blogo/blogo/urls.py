ed8135ce7ed1ad4ef3f409a6d6ac1d072eedf5a3
nipun014 <145190298+nipun014@users.noreply.github.com>
2024-09-16 14:12:41 +0530

    Update urls.py

--- blogo/blogo/urls.py
+++ blogo/blogo/urls.py
@@ -9,10 +9,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("home.urls")),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': 'index'}, name='logout'),
-    path('signup/', views.signup, name='signup')
+    path('',include("home.urls"))
 
     
 ]
