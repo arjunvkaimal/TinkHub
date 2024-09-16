82a5ceee8bc743256b836c249c4156896012f69c
nipun014 <145190298+nipun014@users.noreply.github.com>
2024-09-16 14:13:13 +0530

    login logout signup

--- blogo/home/urls.py
+++ blogo/home/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('blog/new/', views.new_blog, name='new_blog'),
     path('blog/<int:blog_id>/edit/', views.edit_blog, name='edit_blog'),
     path('blog/<int:blog_id>/delete/', views.delete_blog, name='delete_blog'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': 'index'}, name='logout'),
+    path('signup/', views.signup, name='signup')
 
 ]
