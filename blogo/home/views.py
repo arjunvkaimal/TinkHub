36a84f52289d5b4339cc54090249817ec1b08223
nipun014 <145190298+nipun014@users.noreply.github.com>
2024-09-16 16:13:31 +0530

    Update views.py

--- blogo/home/views.py
+++ blogo/home/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -23,39 +24,52 @@ def signup(request):
 @login_required
 def index(request):
     blogs = Blog.objects.filter(author=request.user)  
-    return render(request, 'home/index.html', {'blogs': blogs})
+    return render(request, 'home/index.html', {'blogs':blogs})
 
 
+@login_required
 def new_blog(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user  # Associate the blog with the logged-in user
+            blog.save()
+            return redirect('index')
+    else:
+        form = BlogForm()
+    return render(request, 'home/create.html', {'form': form})
+
+@login_required
+def view_blog(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    return render(request, "home/entry.html", {"blog": blog})
+    if not blog.is_public and request.user != blog.author:
+        return redirect('index')
+  
+@login_required
+def edit_blog(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    if blog.author != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this post")
+    
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
         if form.is_valid():
             form.save()
             return redirect('index')
     else:
-        form = BlogForm()
-    return render(request, "home/create.html", {'form': form})
-
-
-def view_blog(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    return render(request, "home/entry.html", {"blog": blog})
-  
-def edit_blog(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    if request.method == "POST":
-        form = BlogForm(request.POST, instance=blog)
-        if form.is_valid():
-            form.save()
-            return redirect('view_blog', blog_id=blog.id)
-    else:
         form = BlogForm(instance=blog)
-    return render(request, "home/edit.html", {'form': form, 'blog': blog})
+    return render(request, 'home/edit.html', {'form': form})
 
+    
+@login_required
 def delete_blog(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
-    if request.method == "POST":
+    if blog.author != request.user:
+        return HttpResponseForbidden("You are not allowed to delete this post")
+    
+    if request.method == 'POST':
         blog.delete()
         return redirect('index')
-    return render(request, "home/delete.html", {"blog": blog})
-
+    return render(request, 'home/delete.html', {'blog': blog})
